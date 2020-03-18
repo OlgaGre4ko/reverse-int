c0dae0ab92028d99a34dad40029569f1619b6ad9
OlgaGre4ko <osgrechko@mail.ru>
2020-03-18 22:29:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    let mod = Math.abs(n);
+    let str = String(mod);
+    let rev = str.split('').reverse().join('');
+    let res = Number(rev);
+    return (res); 
+};
